abda6104e88209b32c3fbd1ffc6abb47afd6ac36
Delan Azabani <delan@azabani.com>
2014-05-15 16:09:34 +0800

    add OS200_DEBUG macro

--- util.h
+++ util.h
@@ -1,6 +1,12 @@
 #ifndef OS200_UTIL_H
 #define OS200_UTIL_H
 
+#define OS200_DEBUG(...) do { \
+	fprintf(stderr, "%s: ", __func__); \
+	fprintf(stderr, __VA_ARGS__); \
+	fputc('\n', stderr); \
+} while (0)
+
 char *os200_read_line(const char *prompt);
 
 #endif
