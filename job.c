0f334afb79b98781109997882df6e94d8f552df5
Delan Azabani <delan@azabani.com>
2014-05-15 19:41:32 +0800

    tiebreak simultaneous arrivals in file order

--- job.c
+++ job.c
@@ -15,17 +15,6 @@ void os200_job_free(os200_job job) {
 	free(job);
 }
 
-int os200_job_compare_arrival_duration(void *a, void *b) {
-	int result = 0;
-	int arrival_comparison = os200_job_compare_arrival(a, b);
-	int duration_comparison = os200_job_compare_duration(a, b);
-	if (arrival_comparison)
-		result = arrival_comparison;
-	else if (duration_comparison)
-		result = duration_comparison;
-	return result;
-}
-
 int os200_job_compare_arrival(void *a, void *b) {
 	int result = 0;
 	os200_job x = (os200_job) a;
@@ -54,7 +43,7 @@ double os200_job_scan(FILE *input, os200_list output) {
 	while (fscanf(input, "%lf%lf", &arrival, &duration) == 2) {
 		os200_job job = os200_job_new(arrival, duration);
 		os200_list_insert_sorted(output, job,
-			os200_job_compare_arrival_duration);
+			os200_job_compare_arrival);
 	}
 	return quantum;
 }
--- job.h
+++ job.h
@@ -15,8 +15,6 @@ os200_job os200_job_new(double arrival, double duration);
 
 void os200_job_free(os200_job job);
 
-int os200_job_compare_arrival_duration(void *a, void *b);
-
 int os200_job_compare_arrival(void *a, void *b);
 
 int os200_job_compare_duration(void *a, void *b);
