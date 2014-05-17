23382e29c98d8cb611eb94124dd3fa3f81381c36
Delan Azabani <delan@azabani.com>
2014-05-17 19:32:34 +0800

    clearly I didn't test my code

--- util.h
+++ util.h
@@ -3,24 +3,34 @@
 
 #include <pthread.h>
 
-#define OS200_UNUSED(NAME) do { \
-	((void) sizeof(NAME)); \
-} while (0)
+#define OS200_LOCKED_GLOBAL(NAME) \
+	pthread_mutex_t NAME ## _mutex
 
-#define OS200_NEW_SYNCHRONISED_GLOBAL(TYPE, NAME) \
-	TYPE NAME; \
-	OS200_SYNCHRONISED_GLOBAL(NAME)
+#define OS200_LOCKED_EXTERN(NAME) \
+	extern pthread_mutex_t NAME ## _mutex
+
+OS200_LOCKED_EXTERN(stderr);
+
+#define OS200_DEBUG(...) do { \
+	OS200_LOCK(stderr); \
+	fprintf(stderr, "%s: ", __func__); \
+	fprintf(stderr, __VA_ARGS__); \
+	fputc('\n', stderr); \
+	OS200_UNLOCK(stderr); \
+} while (0)
 
 #define OS200_SYNCHRONISED_GLOBAL(NAME) \
 	int NAME ## _ready = 0; \
 	pthread_cond_t NAME ## _cond = PTHREAD_COND_INITIALIZER; \
 	OS200_LOCKED_GLOBAL(NAME)
 
-#define OS200_LOCKED_GLOBAL(NAME) \
-	pthread_mutex_t NAME ## _mutex
+#define OS200_NEW_SYNCHRONISED_GLOBAL(TYPE, NAME) \
+	TYPE NAME; \
+	OS200_SYNCHRONISED_GLOBAL(NAME)
 
-#define OS200_LOCKED_EXTERN(NAME) \
-	extern OS200_LOCKED_GLOBAL(NAME)
+#define OS200_UNUSED(NAME) do { \
+	((void) sizeof(NAME)); \
+} while (0)
 
 #define OS200_WAIT(NAME) do { \
 	os200_wait_real( \
@@ -53,16 +63,6 @@
 #define OS200_UNLOCK_RESET(NAME) do { \
 	OS200_UNLOCK(NAME); \
 	OS200_RESET(NAME); \
-} while (0)
-
-OS200_LOCKED_EXTERN(stderr);
-
-#define OS200_DEBUG(...) do { \
-	OS200_LOCK(stderr); \
-	fprintf(stderr, "%s: ", __func__); \
-	fprintf(stderr, __VA_ARGS__); \
-	fputc('\n', stderr); \
-	OS200_UNLOCK(stderr); \
 } while (0)
 
 typedef enum os200_scheduler {
