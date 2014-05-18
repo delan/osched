c64eece8e636cbaccbd3f7e8949ca111e6b59537
Delan Azabani <delan@azabani.com>
2014-05-19 00:32:00 +0800

    Revert "ready doesn't seem to actually be necessary"
    This reverts commit 3fee8792b5e4a3407fc82709ba3309d2af8acf1f.

--- simulator.c
+++ simulator.c
@@ -11,6 +11,7 @@ OS200_LOCK_NEW(stderr);
 
 OS200_SYNC_NEW(robin);
 OS200_SYNC_NEW(sjf);
+OS200_SYNC_NEW(ready);
 OS200_SYNC_NEW(result);
 
 int quit;
@@ -19,6 +20,7 @@ os200_result result;
 
 void *robin_loop(void *extra) {
 	OS200_SYNC_LOCK(robin);
+	OS200_SYNC_SIGNAL(ready);
 	while (1) {
 		OS200_SYNC_WAIT(robin);
 		OS200_SYNC_RESET(robin);
@@ -32,6 +34,7 @@ void *robin_loop(void *extra) {
 
 void *sjf_loop(void *extra) {
 	OS200_SYNC_LOCK(sjf);
+	OS200_SYNC_SIGNAL(ready);
 	while (1) {
 		OS200_SYNC_WAIT(sjf);
 		OS200_SYNC_RESET(sjf);
@@ -48,8 +51,16 @@ int main(void) {
 	pthread_t robin_thread;
 	pthread_t sjf_thread;
 
+	OS200_SYNC_LOCK(ready);
+
 	pthread_create(&robin_thread, NULL, robin_loop, NULL);
 	pthread_create(&sjf_thread, NULL, sjf_loop, NULL);
+
+	OS200_SYNC_WAIT(ready);
+	OS200_SYNC_RESET(ready);
+
+	OS200_SYNC_WAIT(ready);
+	OS200_SYNC_RESET(ready);
 
 	filename = os200_read_line(prompt);
 	while (strlen(filename) && strcmp(filename, "QUIT")) {
